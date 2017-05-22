7d55170d8d20d480abdd762dec700929689b429c
Winston Elias Romero Duarte <winstonromero@gmail.com>
2017-05-22 14:30:36 -0500

    email 6

--- reubicacion-php_modificar_reubicacion_enviar_email.php
+++ reubicacion-php_modificar_reubicacion_enviar_email.php
@@ -111,11 +111,7 @@
     $mail->addAddress("coordinadorequipoinformatico@uninorte.edu.co", "Coordinador Equipo Informatico");
    // $mail->addAddress("egaliano@uninorte.edu.co", "Emma Beatriz Galiano Vargas");
   //  $mail->AddCC("recepciondeactivos@uninorte.edu.co", "Recepcion de Activos");
-    $mail->AddCC("arellanaa@uninorte.edu.co", "Coordinador Equipo Informatico");
-                ("arellanaa@uninorte.edu.co>", "Alvaro Ivan Santiago Arellana");
-
   //  $mail->AddCC("soportealmacen2@uninorte.edu.co", "Soporte Almacen 2");
-    
     $mail->AddCC("$email_nuevo_responsable", "$nuevo_responsable");
     $mail->AddCC("$email_nuevo_usuario", "$nuevo_usuario");
     $mail->AddCC($_SESSION['correo'], $_SESSION['nombre']);
