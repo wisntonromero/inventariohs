1d22092b122845be162589a3d400de1be3283fab
Winston Elias Romero Duarte <winstonromero@gmail.com>
2017-11-01 10:59:38 -0500

    8

--- arduino-php-ingresar_entrada.php
+++ arduino-php-ingresar_entrada.php
@@ -1,3 +1,38 @@
+
+      
+<?php
+    $server = "localhost";
+    $database = "inventario"; //nombre de la base de datos que estas usando para el proyecto
+    $username = "root"; // nombre del usuario con el que te conectas a esa base de datos
+    $password = "root"; // la password de dicho usuario
+  
+
+    date_default_timezone_set("America/Bogota");
+    header('Content-Type: application/json');
+
+    $conexion = mysql_connect($server,$username,$password);
+    mysql_select_db($database) or die("Oops! Coudn't select Database"); // Select the database
+    mysql_set_charset('utf8',$conexion);
+ 
+    $id_usuario = ($_GET['id_usuario'],ENT_QUOTES);
+    $id_usuario = mysqli_real_escape_string($id_usuario);
+    $permiso    = ($_GET['permiso'],ENT_QUOTES);
+   
+    $query = "INSERT INTO arduino_bitacora(id_usuario, fecha_hora, permiso) VALUES ('".$_GET["id_usuario"]."',now(), '".$_GET["permiso"]."')";  
+      
+    $resultado = mysql_query($query,$conexion);
+    
+    if(mysql_affected_rows()>0){
+        echo "add";
+    }
+    else{
+        echo "2";
+    }
+    mysql_close($conexion);
+?>
+ 
+
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
     <html>
     <head>
@@ -26,7 +61,7 @@
             </div>
         </div>
         <header style="text-align:center";> 
-        <h4>SIPRED - SISTEMA DE INVENTARIO DE PUNTOS DE RED</h4>    
+        <h4>HECHO - SISTEMA DE INVENTARIO DE PUNTOS DE RED</h4>    
         </header>
 
 
