d5297ed1a894ab2573b2cd46ad3b424de3eaa0f0
Winston Elias Romero Duarte <winstonromero@gmail.com>
2017-11-01 12:00:36 -0500

    44

--- arduino-php-ingresar_entrada.php
+++ arduino-php-ingresar_entrada.php
@@ -2,10 +2,10 @@
 <!--      
 <?php
    
-    $conexion = mysql_connect("localhost","root","root");
-    mysql_select_db("inventario", $conexion); // Select the database
+  //  $conexion = mysql_connect("localhost","root","root");
+  //  mysql_select_db("inventario", $conexion); // Select the database
    
-    mysql_query ("INSERT INTO arduino_bitacora('id_usuario') VALUES ('".$_GET["id_usuario"]."')",$conexion); 
+  //  mysql_query ("INSERT INTO arduino_bitacora('id_usuario') VALUES ('".$_GET["id_usuario"]."')",$conexion); 
   //mysql_query =("INSERT INTO arduino_bitacora(id_usuario, fecha_hora, permiso) VALUES ('".$_GET["id_usuario"]."',now(), '".$_GET["permiso"]."')";  
       
    
