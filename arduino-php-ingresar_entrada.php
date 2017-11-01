bae9d8c890f5b6fee2914876fef9b652515ac70e
Winston Elias Romero Duarte <winstonromero@gmail.com>
2017-11-01 13:57:14 -0500

    sss

--- arduino-php-ingresar_entrada.php
+++ arduino-php-ingresar_entrada.php
@@ -13,16 +13,18 @@
  
 
 <?php
-
+$parametro1=$_GET['parametro1'];
 $id_usuario=$_GET['id_usuario'];
-$permiso=$_GET['permiso'];
  
-echo "El Primer Parametro es :" . $id_usuario . "<br />";
+?>
+
+<?php
+echo "El Primer Parametro es :" . $parametro1 . "<br />";
 echo "El Segundo Parametro es :" . $parametro2 . "<br />";
 
   $conexion = mysql_connect("localhost","root","root");
   mysql_select_db("inventario", $conexion); // Select the database
 //  mysql_query ("INSERT INTO arduino_bitacora('id_usuario') VALUES ('".$_GET["parametro1"]."')",$conexion); 
-mysql_query ("INSERT INTO `arduino_bitacora`(`id_usuario`) VALUES ('".$_GET["id_usuario"]."',now(),'".$_GET["permiso"]."')",$conexion);
+mysql_query ("INSERT INTO `arduino_bitacora`(`id_usuario`) VALUES ('".$_GET["id_usuario"]."')",$conexion);
 
 ?>
