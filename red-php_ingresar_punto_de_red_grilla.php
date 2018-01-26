f25ffa7f2ac07cebd479d8911955d2876ce5a82f
Winston Elias Romero Duarte <winstonromero@gmail.com>
2018-01-26 16:41:25 -0500

    tipo_de_punto_de_red3

--- red-php_ingresar_punto_de_red_grilla.php
+++ red-php_ingresar_punto_de_red_grilla.php
@@ -27,7 +27,7 @@ $tablename = $mysqli->real_escape_string(strip_tags($_POST['tablename']));
 $return=false;
 if ( $stmt = $mysqli->prepare("INSERT INTO ".$tablename."  (punto_de_red, bloque, piso,cubiculo, tipo_de_punto_de_red) VALUES (  ?, ?, ?, ?, ?)")) {
 
-	$stmt->bind_param("sss", $punto_de_red, $bloque, $piso);
+	$stmt->bind_param("sss", $punto_de_red, $bloque, $piso, $cubiculo, $tipo_de_punto_de_red);
     $return = $stmt->execute();
 	$stmt->close();
 }             
